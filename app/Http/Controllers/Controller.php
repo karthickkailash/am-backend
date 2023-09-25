6bf3ce03d352f73fd81acdde9c62fcb0d797e7d0
deepikamani74 <deepikait47@gmail.com>
2023-09-25 09:12:49 +0530

    libsodium

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Libsodium\CryptographyPlugin;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +9,44 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+    /**
+    * decryption check.
+    * @param  string  $string
+    * @return mixed
+    */
+    public function decryption()
+    {
+
+        $key = random_bytes(SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_NPUBBYTES);
+       $k=  sodium_bin2hex( $key );
+//print_r($k);
+       // $key   = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES); // 256 bit
+//$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES); // 24 bytes
+//print_r($nonce);
+ $string='0f385921f8283685594e29107da6d898880c0ce15ce46b';
+        if(!empty($string)) {
+           $cryp=new CryptographyPlugin();
+           $data = $cryp->decode($string);
+           //$data1 = json_decode($data);
+           return $data;
+        }
+        return '';
+    }
+    /**
+    * encryption check.
+    * @param  string  $string
+    * @return mixed
+    */
+    public function encryption()
+    {  $string='deepika';
+                if(!empty($string)) {
+            $cryp=new CryptographyPlugin();
+            $data = $cryp->encode($string);
+            return $data;
+        }
+        return '';
+    }
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,12 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->string('employee_id')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('mobile_no')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status')->default('1')->comments('0 - Inactive','1 - Active');
             $table->rememberToken();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
